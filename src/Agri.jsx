0f649e3510da186f6fd6a66b5cae78121d5faf0b
AMAN GUPTA <ag0567688@gmail.com>
2025-09-29 23:10:30 +0530

    first

---
+++ src/Agri.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Agri = () => {
+  return (
+    <div style={{ width: "100%", height: "100vh" }}>
+      <iframe
+        src="https://agrifarmio.lovable.app/"
+        title="Edupath Ai"
+        style={{ width: "100%", height: "100%", border: "none" }}
+      />
+    </div>
+  )
+}
+
+export default Agri
